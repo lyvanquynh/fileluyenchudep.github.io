78ae518bfd7b2ecdcd97cc8811833c72b1c871c2
Lolyphoto Wedding <mrlvq136@gmail.com>
2026-02-08 14:39:46 +0700

    Add files via upload

--- cart.js
+++ cart.js
@@ -316,3 +316,24 @@ if(searchInput){
     })
   })
 }
+// ===== NÚT LÊN ĐẦU TRANG =====
+const backBtn = document.getElementById("backToTop")
+
+if(backBtn){
+
+  window.addEventListener("scroll", ()=>{
+    if(window.scrollY > 300){
+      backBtn.classList.add("show")
+    }else{
+      backBtn.classList.remove("show")
+    }
+  })
+
+  backBtn.addEventListener("click", ()=>{
+    window.scrollTo({
+      top:0,
+      behavior:"smooth"
+    })
+  })
+
+}
